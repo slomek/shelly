9e8f7680ca63e1b69f5390ad86a8a6a875167dfb
Paweł Słomka <pslomka@pslomka.com>
2016-02-08 23:15:42 +0100

    more tests

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
 "use strict";
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 const modeler = require('./lib/modeler'),
     servicer = require('./lib/servicer'),
     routerer = require('./lib/routerer');
 
-module.exports = function(filepath) {
-    let filename = path.basename(filepath),
-        parts = filename.split(/_|\./);
-
-    let def = {
-        name: parts[1],
-        category: parts[0],
-        schemaPath: filepath
-    };
-
-    console.log(def);
-
-    let M = modeler.makeModel(def);
-    let S = servicer.makeService(M);
-    let R = routerer.makeRouter(S);
-
+module.exports = function(pathDir) {
+    let modelFiles = fs.readdirSync(pathDir);
     let router = express.Router();
-    router.use(`/${def.category}/${def.name}`, R);
+
+    modelFiles.forEach(function(filename){
+        let parts = filename.split(/_|\./);
+
+        let def = {
+            name: parts[1],
+            category: parts[0],
+            schemaPath: `${pathDir}/${filename}`
+        };
+
+        let M = modeler.makeModel(def);
+        let S = servicer.makeService(M);
+        let R = routerer.makeRouter(S);
+
+        router.use(`/${def.category}/${def.name}`, R);
+    });
 
     return router;
 };
